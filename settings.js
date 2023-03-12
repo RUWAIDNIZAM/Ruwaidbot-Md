86dad4109d7166cb720640f3346056cee71311e1
RUWAIDNIZAM <80777665+RUWAIDNIZAM@users.noreply.github.com>
2023-03-12 10:28:51 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -31,24 +31,24 @@ global.language = english //change indonesia to english if you don't understand 
 
 // Other
 // Other
-global.botname = "Alphabot-Md" //namabot kalian
-global.ownername = "sandaru" //nama kalian
-global.myweb = "https://s.id/BOTGITHUB" //bebas asal jan hapus
-global.youtube = "https://youtube.com/@sandaru12" //bebas asal jan hapus
+global.botname = "RuwaidBot" //bot owner 
+global.ownername = "RuwaidNizam"
+global.myweb = "https://ruwaidstore.ml"
+global.youtube = "https://www.youtube.com/channel/UC2lLZmD6YmU_VvedSUrgtnQ" //bebas asal jan hapus
 global.myweb2 = "https://s.id/BOTGITHUB" //bebas
-global.email = "mdsandaru2@gmail.com" //bebas
-global.region = "Buddhist" //bebas
+global.email = "nizamruwaid@gmail.com"
+global.region = "India"
 global.timezone = 'Asia/Colombo' //  timezone wib
-global.premium = ['94764497078'] //premium user
-global.owner = ["764497078", "94764497078"] //ganti agar fitur owner bisa di gunakan
+global.premium = ['9942765781'] //premium user
+global.owner = ['942765781']
 global.ownernomer = "764497078" // nomor wa kalian
 global.ownernomerr = "+94764497078" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome = "https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
+global.thumbGif = 'https://example.com'
 global.packname = '© Alphabot-Md' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.author = 'RuwaidNizam'
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.multiplier = 69
